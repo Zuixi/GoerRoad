95e3e1896669eff75d953a8bb8b91161a8ac6205
wqz <wqzhappy@126.com>
2020-08-19 00:25:27 +0800

    complete slice go

--- src/ToGoer/src/basic/slice.go
+++ src/ToGoer/src/basic/slice.go
@@ -2,7 +2,7 @@ package basic
 
 import (
 	"fmt"
-	_"unicode/utf8"
+	"unicode/utf8"
 )
 
 // 引用类型 ：slice, map, channel, function
@@ -121,5 +121,99 @@ func TestSlice() {
 	// ----------------
 	// slice of slice
 	// ----------------
+
+	// 在这我们将name切片取出一部分当做另外一个slice
+	// namecopy 切片的长度是2
+	// 参数是[开始索引: 开始索引 + 长度]，左闭右开
+	// 通过看输出结果，可以知道这两个切片是在共享内存
+	namecopy := name[2: 4]
+	fmt.Printf("\n=> Slice of slice (before)\n")
+
+	inspectSlice(name)
+	inspectSlice(namecopy)
+
+	// 既然是引用，那么如果我们改变namecopy[0]的值，那么name[0]应该也会变化
+	namecopy[0] = "Modify Value"
+
+	// 所以如果需要修改某些切片的值，那么我们就必须时刻知道有谁在用这个切片
+	fmt.Printf("\n=> Slice of slice (after)\n")
+	inspectSlice(name)
+	inspectSlice(namecopy)
+
+	// namecopy := append(namecopy, "CHANGED") 的行为是怎么样的
+	// 如果长度和容量不一样的话，那么append中会有同样的问题产生
+	// 由于namecopy的长度是2，容量是6，所以还有其他空间可以容纳元素
+	// 但是这样将会改变name切片的元素，这不符合常理
+
+	// -----------
+	// 复制一个切片
+	// -----------
+
+	// copy函数只对string和切片有效
+	// 声明一个容量足够的slice，让他能够存储slice2的全部元素
+	// copy 函数的使用
+
+	slice4 := make([]string, len(slice2))
+	copy(slice4, slice2)
+	fmt.Printf("\n=> Copy a slice\n")
+	inspectSlice(slice4)
+
+	// -------------
+	// 切片和引用类型
+	// -------------
+
+	// 声明一个包含5个整数的切片
+	x := make([]int, 5)
+
+	// 随便赋予初值
+	for i := 0; i < 5; i++ {
+		x[i] = i * 100
+	}
+
+	// 将x切片的第二个元素的地址赋予指针
+	pointer := &x[1]
+
+	// 往X中新加一个元素1000， 这将会触发错误
+	// 由于x中长度和容量一样都是5，那么append方法将会扩容一倍
+	// 所以pointer指针将会指向不一样的内存区域
+	x = append(x, 1000)
+
+	// 当我们改变第二个元素的值，pointer指针将不会发生变化，因为指针指向的是旧的x切片
+	// 所以每次使用pointer的值的时候，都会读取到一个错误的值
+	x[1]++
+
+	// 通过查看输出结果，我们就可以看出
+	fmt.Printf("\n=> 切片和引用类型\n")
+	fmt.Println("Pointer: ", *pointer, "\t x[1:] ", x[1])
+
+	// ----------------
+	// UTF-8
+	// ----------------
+	fmt.Printf("\n=> UTF-8\n")
+
+	// go中的字符集都是基于utf-8的
+	// 所以当使用不同的编码的时候，有可能会引发一些其他问题
+
+	// 声明一个字符串，里面包含中文和英语字符
+	// 对于每个中文字符，我们需要3个字节来储存
+	// utf-8建立在字符、代码点、字节三个层面上， go中的string仅仅是字节
+	// 在我们的例子中，前三个字节代表着一个字符
+	// 我们可以从1到4用4个字节表示一个字符(代码点, 一个代码点是32个bit), 所以用多个bit位表示任意一个字符
+	// 在例子中，我用3个字节表示一个字符，因此可以读取字符按照以下方式-->
+	// 3 bytes, 3 bytes, 1 bytes, 1 bytes, ... 
+	str := "世界 means world"
+
+	// UTFMax是4 --  
+	var buf [utf8.UTFMax]byte
+
+	for i, r := range str {
+		r1 := utf8.RuneLen(r)
+
+		si := i + r1
+
+		copy(buf[:], str[i:si])
+
+		fmt.Printf("%2d: %q; codepoint:  %#6x; encoded bytes: %#v\n", i, r, r, buf[:rl])
+	}
 
 } 
